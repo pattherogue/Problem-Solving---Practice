02b165361e8c9e91e0d1c0497630d16acf622a34
Paolo Gomez <peweegardner248@gmail.com>
2024-01-16 09:16:01 -0800

    Update PSP.js

--- PSP.js
+++ PSP.js
@@ -39,6 +39,50 @@ fizzBuzz(100);
 fizzBuzz(16);
 
 //Letter Count
+//You are given a string `word` consisting of alphabetical characters.
+function letterCount (word)
+{
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+//	if word is not a string of alphabetical characters:
+  if (typeof word !== "string")
+  {
+    //	print Error and stop
+    console.error("Error");
+    return;
+  }
+
+  for (const letter of word)
+  {
+    if (!alphabet.includes(letter.toLowerCase()))
+    {
+      console.error("Error");
+      return;
+    }
+  }
+
+  let letterCounts = {};
+
+  for (let letter of word)
+  {
+    let lowerCaseLetter = letter.toLowerCase();
+
+    // If the letter is already in the dictionary, increment its count
+    if (letterCounts[lowerCaseLetter] !== undefined)
+    {
+      letterCounts[lowerCaseLetter]++;
+    }
+    else
+    {
+      letterCounts[lowerCaseLetter] = 1;
+    }
+  }
+
+  return letterCounts;
+}
+
+letterCount("Caesar42");
+console.log(letterCount("Caesar"));
+console.log(letterCount("AAbaBa"));
 
 
 
